3439ce88a4fdade551861cd3ebe349f1973c93ed
teja <48879182+shade8@users.noreply.github.com>
2023-10-09 17:55:19 +0530

    Add files via upload

---
+++ userAPI/UserAPI/Controllers/UserController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UserAPI.Data;
+
+namespace UserAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UserController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<User>>> GetUsers()
+        {
+            return Ok(await _context.Users.ToListAsync());
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<User>>> CreateUser(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Users.ToListAsync());
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<User>>> UpdateUser(User user)
+        {
+            var dbUser = await _context.Users.FindAsync(user.Id);
+            if (dbUser == null)
+                return BadRequest("Hero not found.");
+
+            dbUser.Name = user.Name;
+            dbUser.FirstName = user.FirstName;
+            dbUser.LastName = user.LastName;
+            dbUser.Place = user.Place;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Users.ToListAsync());
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<User>>> DeleteUser(int id)
+        {
+            var dbUser = await _context.Users.FindAsync(id);
+            if (dbUser == null)
+                return BadRequest("Hero not found.");
+
+            _context.Users.Remove(dbUser);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Users.ToListAsync());
+        }
+    }
+}
---
+++ userAPI/UserAPI/Data/DataContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace UserAPI.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<User> Users => Set<User>();
+    }
+}
